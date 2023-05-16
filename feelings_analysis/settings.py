1e7ad4d9efece6211e0904a3eafc68c6844dd313
عبدالعزيز باحميد <gimo999@hotmail.com>
2023-05-16 10:55:33 +0300

    changed the server to django and added some code with python libraries

--- feelings_analysis/settings.py
+++ feelings_analysis/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-88-o)%r$n2br2du0-yim8gs2v8mkmu+!ab(bs_#j$2&p-uo3-8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
